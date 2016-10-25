10b9a2ec2bd49ee8f0117056664bee72034d28ac
Christian Rasmussen <christian.rasmussen@me.com>
2016-10-25 20:54:32 +0200

    Added code that displays the problem.

--- ReactiveCocoa-Trigger/ViewController.swift
+++ ReactiveCocoa-Trigger/ViewController.swift
@@ -1,25 +1,30 @@
-//
-//  ViewController.swift
-//  ReactiveCocoa-Trigger
-//
-//  Created by Christian Rasmussen on 25.10.2016.
-//  Copyright © 2016 Rasmussen I/O. All rights reserved.
-//
-
 import UIKit
+import ReactiveSwift
+import ReactiveCocoa
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
+        let config = URLSessionConfiguration.default
+        let urlSession = URLSession(configuration: config)
+
+        let request = URLRequest(url: Bundle.main.url(forResource: "Test", withExtension: "txt")!)
+
+        for i in 1...200 {
+            print("Requesting: #\(i)")
+            let task = urlSession.reactive.data(with: request)
+
+            task.startWithCompleted {
+                print("Completed: #\(i)")
+            }
+
+            // This is the problematic code. Remove comment from line 26 to introduce bug.
+            let viewController = UIViewController()
+//            _ = viewController.reactive.trigger(for: #selector(UIViewController.viewDidAppear(_:)))
+            // Observing the returned signal does not change anything. This one line is enough to break the URLSession.
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
