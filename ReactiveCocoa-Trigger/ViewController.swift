e58cf296713074b651264fb1537b864d2833a5e1
Christian Rasmussen <christian.rasmussen@me.com>
2016-10-25 20:38:53 +0200

    Initial commit.

---
+++ ReactiveCocoa-Trigger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactiveCocoa-Trigger
+//
+//  Created by Christian Rasmussen on 25.10.2016.
+//  Copyright © 2016 Rasmussen I/O. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
